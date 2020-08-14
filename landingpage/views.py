0853833daa823c5805f5fbb9ea2de9a51c93de75
Necro <andre.kramp@necro-development.net>
2020-08-15 01:55:59 +0200

    landing page venue partners in view, only show partners when available

--- landingpage/views.py
+++ landingpage/views.py
@@ -4,6 +4,12 @@ from partners.models import Partner
 # landingpage
 def index(request):
     partners = Partner.objects.filter(hide=False).order_by("order")
-    return render(request, "index.html", {'partners': partners})
+    return render(
+        request,
+        "index.html",
+        {
+            'venue_partners': [partner for partner in partners if partner.bbb],
+            'venueless_partners': [partner for partner in partners if not partner.bbb] # lul :3
+        })
 
 
