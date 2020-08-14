b2fb77c8f2b19f9ed56136f34f898438198bfe02
Lukas Schauer <lukas@schauer.so>
2020-08-14 16:14:25 +0000

    removed custom user admin (relict from template project)

--- authstuff/admin.py
+++ authstuff/admin.py
@@ -3,21 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 
 from . import models
 
-class CustomUserAdmin(UserAdmin):
-    def get_fieldsets(self, request, obj=None):
-        fieldsets = []
-        fieldsets += [(None, {'fields': ('username', 'password', 'is_active')})]
-        fieldsets += [('Optional', {'fields': ('email', )})]
-
-        fieldsets += (
-            ('Personal info', {'fields': ('first_name', 'last_name',)}),
-            ('Permissions', {'fields': ('is_staff', 'is_superuser', )}),
-            ('Important dates', {'fields': ('date_joined', 'last_login',)}),
-        )
-
-        return fieldsets
-
-admin.site.register(models.User, CustomUserAdmin)
+admin.site.register(models.User, UserAdmin)
 
 admin.site.register(models.AuthToken)
 
