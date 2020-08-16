90bcd217998da5cb6d60e70edf057dde0a302297
Lukas Schauer <lukas@schauer.so>
2020-08-16 19:13:34 +0200

    fix choices compatibility with older django versions

--- chat/models.py
+++ chat/models.py
@@ -5,14 +5,15 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 
-class MessageType(models.TextChoices):
-    SYSTEM = 'SYS', _('system_message')
-    MESSAGE = 'MSG', _('chat_message')
-    WHISPER = 'WPR', _('whisper_message')
+messagetypes = (
+    ('SYS', _('system_message')),
+    ('MSG', _('chat_message')),
+    ('WPR', _('whisper_message')),
+)
 
 class Message(models.Model):
 
-    type = models.CharField(max_length=3, choices=MessageType.choices, default=MessageType.MESSAGE)
+    type = models.CharField(max_length=3, choices=messagetypes, default='MSG')
     date = models.DateTimeField(auto_now_add=True)
     # @TODO Fix magix number for maximum username length
     room = models.CharField(max_length=30)
@@ -29,7 +30,7 @@ class Message(models.Model):
             channel_layer = channels.layers.get_channel_layer()
             async_to_sync(channel_layer.group_send)('chat_%s' % self.room, 
                 {
-                    'type': str(MessageType(self.type).label),
+                    'type': self.type,
                     'message': self.chatmsg()
                 })
         
